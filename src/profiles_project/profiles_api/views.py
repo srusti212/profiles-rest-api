8b45cf5e35353a1bd6d938bec78f968046f56737
srusti212 <srusti212@gmail.com>
2019-05-05 13:04:50 -0700

    Added POST, PUT, PATCH and DELETE to our APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,12 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 
 # APIViews work by defining functions that match the standard Http Methods
 class HelloApiView(APIView):
     """ Test API View """
+
+    """ Telling DRF, the serializer we want to use to describe the data that we handle with this API view.
+    So right below the dock string above the get function let's add a serializer variable and let's reference our serializer. """
+
+    """                filename/classname"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Returns a list of APIView features """
@@ -24,3 +32,30 @@ class HelloApiView(APIView):
         So the Response object is effectively a dictionary which is converted to json and output to the screen """
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    # request contains the data sent through post()
+    def post(self, request):
+        """ Create a hello message with our name """
+        """ This post to APIView returns a message that includes a name that was posted to the API. """
+        # Base the serializer on the data that was sent through post() to the API
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        """ Checks if the serializer has valid data passed into. """
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ Handles updating an object. """
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """ Patch request, only updates fields provided in the request. """
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """ Deletes an object. """
+        return Response({'method': 'delete'})
